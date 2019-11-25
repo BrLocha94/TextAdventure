3d4d50c6efd5a15084925c78a45e3e3cb9197859
BrLocha94 <brunolocha2@gmail.com>
2019-11-25 11:55:29 -0300

    Created Node class

---
+++ TextAdventure/Assets/Scripts/Utils/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private Node frontPath;
+    [SerializeField]
+    private Node backPath;
+    [SerializeField]
+    private Node leftPath;
+    [SerializeField]
+    private Node rightPath;
+
+    private string description;
+
+    private List<Interacteble> possibleInteractions;
+    private List<Action> possibleActions;
+
+    void Start()
+    {
+        if (possibleInteractions == null)
+            possibleInteractions = new List<Interacteble>();
+
+        if (possibleActions == null)
+            possibleActions = new List<Action>();
+    }
+}
