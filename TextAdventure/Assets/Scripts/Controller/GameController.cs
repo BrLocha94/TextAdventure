c24d2747e18d554ad38c2476d59542e5aa5bcc66
BrLocha94 <brunolocha2@gmail.com>
2019-12-03 23:29:52 -0300

    Add lean gui to the project, updated project interface to new UX strategies

---
+++ TextAdventure/Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean;
+
+public class GameController : MonoBehaviour
+{
+    public Lean.Gui.LeanWindow block;
+    public Lean.Gui.LeanWindow popupMenu;
+
+    bool toogleState = false;
+
+	void Start ()
+    {
+		
+	}
+	
+    public void PopMenu()
+    {
+        toogleState = !toogleState;
+
+        block.GetComponent<CanvasGroup>().blocksRaycasts = toogleState;
+        block.Toggle();
+        popupMenu.Toggle();
+    }
+
+    
+}
---
+++ TextAdventure/Assets/Scripts/Utils/ButtonHolder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean;
+
+public class ButtonHolder : MonoBehaviour
+{
+    bool is_over = false;
+    bool count = false;
+    float time = 0;
+
+	void Update ()
+    {
+        if (is_over == false && count == true)
+        {
+            time += Time.deltaTime;
+            if (time >= 5f)
+                OutTransition();
+        }
+	}
+
+    public void InTransition()
+    {
+        gameObject.GetComponent<CanvasGroup>().interactable = true;
+        gameObject.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        gameObject.GetComponent<Lean.Gui.LeanWindow>().TurnOn();
+
+        is_over = false;
+        count = true;
+        time = 0f;
+    }
+
+    public void OutTransition()
+    {
+        gameObject.GetComponent<CanvasGroup>().interactable = false;
+        gameObject.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        gameObject.GetComponent<Lean.Gui.LeanWindow>().TurnOff();
+
+        is_over = false;
+        count = false;
+        time = 0f;
+    }
+
+    public void NormalTransition()
+    {
+        is_over = false;
+        time = 0f;
+    }
+
+    public void DownTransition()
+    {
+        is_over = true;
+        time = 0f;
+    }
+}
