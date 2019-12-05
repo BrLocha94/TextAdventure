0527ebdd6cc51586db7499adaf02bb6e4c2d7026
BrLocha94 <brunolocha2@gmail.com>
2019-12-04 23:58:39 -0300

    Created all actions off the game

--- TextAdventure/Assets/Scripts/_interfaces/Action.cs
+++ TextAdventure/Assets/Scripts/_interfaces/Action.cs
@@ -1,7 +1,27 @@
 ﻿
 public interface Action
 {
-    bool Randomize();
+    Results Randomize();
 
-    void Execute();
+    Results Execute();
+
+    ActionType GetActionType();
+}
+
+public enum Results
+{
+    OUTSTANDING,
+    SUCESS,
+    FAIL,
+    CATASTROFIC
+}
+
+public enum ActionType
+{
+    CLOSE,
+    CUT,
+    INSPECT,
+    OPEN,
+    PIC,
+    THROW
 }
---
+++ TextAdventure/Assets/Scripts/_interfaces/ExtendedClasses/Action/Open.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour, Action
+{
+    private ActionType type = ActionType.OPEN;
+
+    int outstandingSucces = 10;
+    int successRate = 50;
+    int failRate = 90;
+    int catastroficFailRate = 100;
+
+    public Results Randomize()
+    {
+        int random = Random.Range(0, 100);
+
+        if (random <= outstandingSucces)
+            return Results.OUTSTANDING;
+        else if (random <= successRate)
+            return Results.SUCESS;
+        else if (random <= failRate)
+            return Results.FAIL;
+        else
+            return Results.CATASTROFIC;
+    }
+
+    public Results Execute()
+    {
+        return Randomize();
+    }
+
+    public ActionType GetActionType()
+    {
+        return type;
+    }
+}
